7e14bb1ee207535f561fe110b784df84206cc2ac
Guru Preetam Bodapati <gurupreetambodapati@gmail.com>
2025-05-15 17:40:03 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.cgpa',
+  appName: 'CGPA Calculator CSM',
+  webDir: 'public'
+};
+
+export default config;
